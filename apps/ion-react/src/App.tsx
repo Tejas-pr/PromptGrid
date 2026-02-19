57e8f7827c4d3c95c64cfd87b51e101ab8bbf864
Tejas <tejas.teju02@gmail.com>
2026-02-19 23:56:48 +0530

    react template

---
+++ apps/ion-react/src/App.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router";
+import Home from "./Home";
+import LoginPage from "./components/auth/login";
+import SignupPage from "./components/auth/signup";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ apps/ion-react/src/Home.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello world</h1>
+        <Button>Shadcn button</Button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
